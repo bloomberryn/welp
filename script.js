0e0cae275d2ec79c3f0238b5ae7784461d3c044b
bloomberryn <64485249+bloomberryn@users.noreply.github.com>
2023-11-26 10:42:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showShyGif() {
+    document.getElementById('gif').src = 'shy.gif';
+    document.getElementById('message').innerText = 'Yeyyyyyyyyy, I wuv youuuu!';
+    document.getElementById('noButton').disabled = true; // Disable the 'No' button after 'Yes' is clicked
+}
