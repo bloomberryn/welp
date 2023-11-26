f9fc8325618777af8328ffb89e3b52d5d741aa72
bloomberryn <64485249+bloomberryn@users.noreply.github.com>
2023-11-26 10:52:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function showShyGif() {
-    document.getElementById('gif').src = 'shy.gif';
-    document.getElementById('message').innerText = 'Yeyyyyyyyyy, I wuv youuuu!';
-    document.getElementById('noButton').disabled = true; // Disable the 'No' button after 'Yes' is clicked
+    const question = document.querySelector(".question");
+    const gif = document.querySelector(".gif");
+    const noBtn = document.querySelector(".no-btn");
+
+    question.innerHTML = "Yeeyyyyy I wuvv youuu!";
+    gif.src = "shy.gif";
+    noBtn.disabled = true; // Disable the 'No' button after 'Yes' is clicked
+}
+
+function moveNoButton() {
+    const noBtn = document.querySelector(".no-btn");
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
 }
